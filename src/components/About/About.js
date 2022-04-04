af760fef2a07f78a9c36f28bd903c1f52a173fb7
azirulislam <azirulislam27@gmail.com>
2022-04-05 00:52:13 +0600

    blog content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>this is about</h2>
+            <h3>Here You can Know More About us</h3>
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>this is blogs</h2>
+        <div className='blogs'>
+            <h3>About context API</h3>
+            <p>The React context is API is a way for React app to effectively produce global variable that can be passed around.It major solver of prop drilling.Context API is a React hook.It returns a consumer and provider.It can Store all data in parent component. </p>
+            <h3>About semantic tag.</h3>
+            <p>Semantic tag using is the more important in modern website.Header, nav, article, aside,section,footer is the main semantic tag.Using these tags makes a website very easily.Article tag wrap the all article and nav tag wrap the navbar in a website.At a result a developer can read the code very easily. </p>
         </div>
     );
 };
