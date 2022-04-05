bccde3a556e2e8d977c5f167ae993f74ad340156
azirulislam <azirulislam27@gmail.com>
2022-04-05 15:15:56 +0600

    one chart added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,82 @@
 import React from 'react';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+  } from "recharts";
+  const data = [
+    {
+      name: "Mar",
+      uv: 100000,
+      pv: 241,
+      amt: 10401
+    },
+    {
+      name: "Apr",
+      uv: 200000,
+      pv: 423,
+      amt: 24500
+    },
+    {
+      name: "May",
+      uv: 500000,
+      pv: 726,
+      amt: 67010
+    },
+    {
+      name: "June",
+      uv: 500000,
+      pv: 529,
+      amt: 40405
+    },
+    {
+      name: "July",
+      uv: 600000,
+      pv: 601,
+      amt: 50900
+    },
+    {
+      name: "Aug",
+      uv: 700000,
+      pv: 670,
+      amt: 61000
+    },
+  
+  ];
+  
+
+
 
 const Dashboard = () => {
     return (
-        <div>
-            <h5>rhis is dashboard</h5>
-        </div>
+        <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,14 +4,15 @@ import './Home.css';
 const Home = () => {
     return (
         <div>
-            <h2>Wellcome to BMW Car</h2>
+            <h2>Wellcome to BMW CAR Market</h2>
             <div  className='container'>
             <div className='car-image'><img src="bmw.jpg" alt="" /></div>
             <div>
                 <p>BMW, in full Bayerische Motoren Werke AG, German automaker noted for quality sports sedans and motorcycles. Headquarters are in Munich.It originated in 1916 as Bayerische Flugzeug-Werke, a builder of aircraft engines, but assumed the name Bayerische Motoren Werke in July 1917 and began producing motorcycles in the 1920s. </p>
+
+                <button className='car-btn'>Know-more</button>
             </div>
             </div>
-            <button className='car-btn'>Know-more</button>
         </div>
     );
 };
