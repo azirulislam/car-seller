5f1fc16f88840398327ad6072a62bdf7a52468c4
azirulislam <azirulislam27@gmail.com>
2022-04-05 21:46:00 +0600

    ok

--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -3,8 +3,37 @@ import React from 'react';
 const Review = () => {
     return (
         <div>
-            <h2>this is review</h2>
-        </div>
+                <div className='review'>
+                <h3>Gates Silva</h3>
+                <h5>4 Star</h5>
+                <p>I have owned my 2013 BMW 535i for 7 years now and love the car! Great performance, looks and fuel efficiency. The water pump recently blew on it at 60K but I heard that is typically the ONLY thing that fails on this car.</p>
+                </div>
+                <div className='review'>
+                <h3>Wong Travis</h3>
+                <h5>5 Star</h5>
+                <p>The appearance of the 330is is way off. The reason why we as public is going wild over the e30 325is is because of its boxy body shape as it is to perfect, the amazing sound of the M20 2.7 motor and interior.</p>
+                </div>
+                <div className='review'>
+                <h3>Valdez Meclure</h3>
+                <h5>3 Star</h5>
+                <p>I have a BMW 435i and just had to put it in the shop for drivetrain/engine malfunction for the 2nd time. This will, no doubt, be another $2k. BMW's are by far the most overrated, unreliable cars in the world.</p>
+                </div>
+                <div className='review'>
+                <h3>Schwartz Vincent</h3>
+                <h5>5 Star</h5>
+                <p>My speedometer in my brand new car is off by 10% and when I brought it in to BMW Spokane for correction, was told BMW has a disclaimer for accuracy of their speedometers.</p>
+                </div>
+                <div className='review'>
+                <h3>Rayan Burns</h3>
+                <h5>2 Star</h5>
+                <p>Bought a 2020 M340i xdrive BMW. The proximity feature of the Idrive have been an issue from the day I picked up the vehicle. On the way home from the dealership the doors would not open using the door handles.</p>
+                </div>
+                <div className='review'>
+                <h3>Pieter Handscob</h3>
+                <h5>4 Star</h5>
+                <p>Brilliant all the way. The model 530i series is a very promising drive without the fuel as an exception. However a technological engineering marvel. I never see why you need a driver for this vehicle by the owner.</p>
+                </div>
+                </div>
     );
 };
 
